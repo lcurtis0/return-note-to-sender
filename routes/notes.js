aae24b6f2b524177acb23f37c555b8b476f166dc
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-08-13 14:48:39 -0400

    removed router line

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const router = express.Router();
+//const router = express.Router();
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'notes.html'));
