de3858c59d0ad8b04ed5d084f027a6ce35ef5edb
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-08-12 20:49:17 -0400

    added return

--- routes/notes.js
+++ routes/notes.js
@@ -10,4 +10,8 @@ app.get('/', (req, res) => {
     res.readfromfile('db/db.json').then((data)=>{res.json(JSON.parse(data))})
   })
 
+  app.post('/api/notes', (req, res) => {
+    res.json(`${req.method} request received`);
+  })
+
  module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 
-
-
 const express = require('express');
 const path = require('path');
 const data = require('./db/db.json'); 
@@ -13,22 +11,21 @@ const notes = require('./notes.html');
 const index = require('./index.html');
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'notes.html'));
-})
-
-app.get('api/notes', (req, res)=>{
-  res.readfromfile('db/db.json').then((data)=>{res.json(JSON.parse(data))})
+  return res.sendFile(path.join(__dirname, 'notes.html'));
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  return res.sendFile(path.join(__dirname, 'index.html'));
 });
+
 
 
 const notesRouter = require('./routes/notes.js');
 
 app.use('/notes', notesRouter);
 
+app.listen(PORT, ()=>
+console.log(`hello PORT is working ${PORT}`))
 
 module.exports = app;
 
